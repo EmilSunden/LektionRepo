0b509d4a13a9e48ed296522b609833c5b0451c8c
EmilSunden <emilsunjobb@gmail.com>
2022-09-19 20:26:06 +0200

    yoyoyoyo

--- emil.js
+++ emil.js
@@ -11,22 +11,23 @@ let makaronerLagning = () => {
     if (consent !== "ja") {
         console.log("Okej bai! ")
     } else {
-        antalPortion = prompt("Då fortsätter vi! Hur många portioner pasta vill du ha? ")
+        console.log("Great! ")
     }
 
-
-    if (antalPortion === "1"){
-        console.log(`Till en portion behöver du ${makaronerGram} gram snabbmakaroner vilket är ${makaronerDeciliterPerPortion} deciliter`)
-    } else if (antalPortion === "2"){
-        console.log(`Till två portioner behöver du ${makaronerGram * 2} gram snabbmakaroner vilket är ${makaronerDeciliterPerPortion * 2} deciliter`)
-    } else if (antalPortion === "3"){
-        console.log(`Till tre portioner behöverdu ${makaronerGram * 3} gram snabbmakaroner vilket är ${makaronerDeciliterPerPortion * 3} deciliter`)
-    } else {
-        throw "404 Error"
-    }
-
-   
-
-
-};
+    antalPortion = prompt("Då fortsätter vi! Hur många portioner pasta vill du ha? ")
+    while (antalPortion !== "1" && antalPortion !== "2" && antalPortion !== "3"){
+        if (antalPortion !== "1" && antalPortion !== "2" && antalPortion !== "3"){
+            antalPortion = prompt("Not allowed! ")
+            } else if (antalPortion === "1"){
+                console.log(`Till en portion behöver du ${makaronerGram} gram snabbmakaroner vilket är ${makaronerDeciliterPerPortion} deciliter`)
+            } else if (antalPortion === "2"){
+                console.log(`Till två portioner behöver du ${makaronerGram * 2} gram snabbmakaroner vilket är ${makaronerDeciliterPerPortion * 2} deciliter`)
+            } else if (antalPortion === "3"){
+                console.log(`Till tre portioner behöver du ${makaronerGram * 3} gram snabbmakaroner vilket är ${makaronerDeciliterPerPortion * 3} deciliter`)
+            } else {
+                break;
+            }
+        
+        }
+    };
 makaronerLagning()
