cf249542f31d09338854a650fb033dd659b212c6
Kenneth <kenneth.ellebring@yh.nackademin.se>
2022-09-19 14:11:19 +0200

    addat pkgjson och lagt till kod i ken1

--- ken1.js
+++ ken1.js
@@ -1,1 +1,15 @@
 const prompt = require('prompt-sync')()
+
+
+function kokaMaks(){
+    const portioner = prompt("Hur många portioner pasta vill du göra: "); // hur många portioner i en prompt
+//console.log(portioner)
+    let liter = portioner * 1;
+    let salt = port
+console.log(`Du har valt att göra ${portioner} med pasta,
+du behöver fylla en kastrull med ca: ${liter}liter vatten och tillsätta`);
+
+
+}
+
+kokaMaks()
