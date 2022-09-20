42f737dd4e0f3b152b1bec4e30e1cb02325ce024
KennethEllebring <kenneth.ellebring@yh.nackademin.se>
2022-09-20 13:55:07 +0200

    lagat koden och lagt på en fungerande do/while loop

--- ken1stuff/ken1.js
+++ ken1stuff/ken1.js
@@ -1,53 +1,89 @@
-const prompt = require('prompt-sync')() // problem med prompt-sync biblioteket, stödjer inte /n newline
 
+// Bibliotek //
+
+const prompt = require('prompt-sync')() // problem med prompt-sync biblioteket, stödjer inte /n newline därför den kopierar text hela tiden när man trycker en siffra
+const readlineSync = require('readline-sync');
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-q = `Välkommen till kokboken välj ett alternativ med siffrorna:
+///////////////////////////////////
+//                               //
+//            input              //
+//                               //
+///////////////////////////////////
+
+let loop = 1;
+
+const q = `
+Välkommen till kokboken välj ett alternativ med siffrorna:
+
 1. Koka Snabbmakroner
-2. ***inte klart***
+2. Måttkonverterare
 3. ***inte klart***
+
 0. för att avbryta: 
 --> `;
 
+function kokaMaks(){
+  portioner = prompt("Hur många portioner snabbmakaroner vill du göra? --> ")
+  let liter = portioner * 0.5;
+  let pasta = portioner * 2;
+  let salt = portioner * 0.5; 
+  console.log(`
+  Du har valt att göra ${portioner} portioner med pasta,
+  
+  du behöver fylla en kastrull med ca: ${liter} l vatten, 
+  tillsätt ${salt} tsk salt, vänta tills vattnet kokar och häll sedan
+  i ${pasta} dl pasta och vänta ca 3 min, häll av i ett durkslag och servera,
+  bon apitit!`);
+                   }
 
-//kokboken = prompt(q);
 
-rl.question(q, function(kokboken) {
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
+//                            Program                                        //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+
+do{
+    var kokboken = readlineSync.question(q)
+
+    switch(parseInt(kokboken)){
+      case 1 : 
+        kokaMaks();
+        break;
+
+      case 2 : 
+        conVolym();
+        break;
+
+      case 3 :
+        kokaMaks();
+        break;
+
+      case 0 : 
+        loop = 0, console.log("Avbryter");
+        break;
+
+      default: console.log("fel knapp prova igen")
+    }; 
+} while(loop > 0)
+
   
 
-switch(kokboken){
-    case "1" : kokaMaks();
-    break;
-
-    case "2" : kokaMaks();
-    break;
-
-    case "3" : kokaMaks();
-    break;
-
-    case "4" : 
-    break;
-}
 
 
-function kokaMaks(){
-    portioner = prompt("Hur många portioner snabbmakaroner vill du göra? --> ")
-    let liter = portioner * 0.5;
-    let pasta = portioner * 2;
-    let salt = portioner * 0.5; 
-console.log(`
-Du har valt att göra ${portioner} portioner med pasta,
-
-du behöver fylla en kastrull med ca: ${liter} l vatten, 
-tillsätt ${salt} tsk salt, vänta tills vattnet kokar och häll sedan
-i ${pasta} dl pasta och vänta ca 3 min, häll av i ett durkslag och servera,
-bon apitit!`);
-
-}
-
-rl.close()  // för att stänga readline
+ /*
+// Wait for user's response.
+var userName = readlineSync.question('May I have your name? ');
+console.log('Hi ' + userName + '!');
+ 
+// Handle the secret text (e.g. password).
+var favFood = readlineSync.question('What is your favorite food? ', {
+  hideEchoBack: true // The typed text on screen is hidden by `*` (default).
 });
+console.log('Oh, ' + userName + ' loves ' + favFood + '!')*/
